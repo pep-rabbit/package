87952fc1cb5c0aab10d3c9f619b0158bf4234877
Nazar <self.coft@gmail.com>
2026-04-14 12:36:45 +0300

    Refactor main.py to inline CORS configuration for improved readability

--- DeLiki/src/main.py
+++ DeLiki/src/main.py
@@ -2,10 +2,7 @@ from litestar import Litestar
 from litestar.config.cors import CORSConfig
 from src.endpoin import router
 
-app = Litestar(route_handlers=[router])
-cors_config = CORSConfig(allow_origins=["*"]) 
-
 app = Litestar(
     route_handlers=[router],
-    cors_config=cors_config
+    cors_config=CORSConfig(allow_origins=["*"])
 )
